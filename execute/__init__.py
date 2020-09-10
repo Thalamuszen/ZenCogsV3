73001626c7dac0f8dceac7c44b9cc4b527a003fa
Thalamuszen <thalamuszen@hotmail.com>
2020-09-10 23:41:21 +0100

    Update __init__.py

--- execute/__init__.py
+++ execute/__init__.py
@@ -1,6 +1,7 @@
 from .execute import execute
 
+__red_end_user_data_statement__ = "This cog does not store any End User Data. Author IDS are stored when creating an embed, and this cannot be deleted to keep authors accountable for the embeds they store."
 
 def setup(bot):
-    n = execute()
+    n = execute(bot)
     bot.add_cog(n)
