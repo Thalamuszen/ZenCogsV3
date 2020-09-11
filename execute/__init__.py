d498db005723132171d1f7c4fb8e85fab2892a9d
Thalamuszen <thalamuszen@hotmail.com>
2020-09-11 11:28:21 +0100

    Update __init__.py

--- execute/__init__.py
+++ execute/__init__.py
@@ -5,9 +5,5 @@ from redbot.core.bot import Red
 
 from .execute import Execute
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
 async def setup(bot: Red) -> None:
     bot.add_cog(Execute(bot))
