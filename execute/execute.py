a955f878fdc8cf06b50e554c18a9feeb5b0f01c8
Thalamuszen <thalamuszen@hotmail.com>
2020-09-11 11:33:35 +0100

    Update execute.py

--- execute/execute.py
+++ execute/execute.py
@@ -29,23 +29,6 @@ class Execute(commands.Cog):
 
     @checks.is_owner()
     @commands.command()
-    async def do(self, ctx, times: int, *, command):
-        """Repeats a command a specified number of times."""
-        msg = copy(ctx.message)
-        msg.content = ctx.prefix + command
-
-        new_ctx = await self.bot.get_context(msg, cls=type(ctx))
-        #new_ctx._db = ctx._db
-
-        try:
-            for i in range(times):
-                await new_ctx.reinvoke()
-                await asyncio.sleep(1)
-        except Exception as e:
-            await ctx.send(embed=discord.Embed(title="Oops!", description=f"```\n{e}\n```"))
-
-    @checks.is_owner()
-    @commands.command()
     async def execute(self, ctx, sequential: typing.Optional[bool] = False, *, commands):
         """Execute multiple commands at once. Split them using |."""
         commands = commands.split("|")
@@ -61,44 +44,3 @@ class Execute(commands.Cog):
                 new_message.content = ctx.prefix + command.strip()
                 todo.append(self.bot.process_commands(new_message))
             await asyncio.gather(*todo)
-
-    @checks.is_owner()
-    @commands.command()
-    async def bypass(self, ctx, *, command):
-        """Bypass a command's checks and cooldowns."""
-        msg = copy(ctx.message)
-        msg.content = ctx.prefix + command
-
-        new_ctx = await self.bot.get_context(msg, cls=type(ctx))
-        try:
-            await new_ctx.reinvoke()
-        except Exception as e:
-            await ctx.send(embed=discord.Embed(title="Oops!", description=f"```\n{e}\n```"))
-
-    @checks.is_owner()
-    @commands.command()
-    async def timing(self, ctx: commands.Context, *, command_string: str):
-        """
-        Run a command timing execution and catching exceptions.
-        """
-        msg = copy(ctx.message)
-        msg.content = ctx.prefix + command_string
-        alt_ctx = await self.bot.get_context(msg, cls=type(ctx))
-
-        #alt_ctx = await copy_context_with(ctx, content=ctx.prefix + command_string)
-
-        if alt_ctx.command is None:
-            return await ctx.send(f'Command "{alt_ctx.invoked_with}" is not found')
-
-        start = time.perf_counter()
-
-        await alt_ctx.reinvoke()
-
-        #async with ReplResponseReactor(ctx.message):
-        #    with self.submit(ctx):
-        #        await alt_ctx.command.invoke(alt_ctx)
-
-        end = time.perf_counter()
-        return await ctx.send(
-            f"Command `{alt_ctx.command.qualified_name}` finished in {end - start:.3f}s."
-        )
