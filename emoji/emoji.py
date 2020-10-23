bddb9ee5d3d67507a375fbe5021b3511f4aaa898
Thalamuszen <thalamuszen@hotmail.com>
2020-10-23 23:25:37 +0100

    Add files via upload

---
+++ emoji/emoji.py
@@ -0,0 +1,52 @@
+import re
+
+from redbot.core import checks, Config, commands
+
+PATTERN = re.compile(r"w+h*[aou]+t+[?!]*", re.IGNORECASE)
+
+
+class Emoji(commands.Cog):
+
+    """Enlarges someone another users Emoji"""
+
+    default_global_settings = {"channels_ignored": [], "guilds_ignored": []}
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.conf = Config.get_conf(self, identifier=527690525)
+        self.conf.register_global(**self.default_global_settings)
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+
+    @commands.group(name="emojiignore", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_guild=True)
+    async def emojiignore(self, ctx):
+        """Change Emoji cog ignore settings."""
+        pass
+
+    @commands.command()
+    async def emoji(self, message):
+        if message.guild is None:
+            return
+        if message.author.bot:
+            return
+        content = message.content.lower().split()
+        if len(content) != 1:
+            return
+
+        if PATTERN.fullmatch(content[0]):
+            async for before in message.channel.history(limit=5, before=message):
+                author = before.author
+                name = author.display_name
+                content = before.clean_content
+                if (
+                    not author.bot
+                    and not author == message.author
+                    and not PATTERN.fullmatch(content)
+                ):
+                    emoji = "\N{CHEERING MEGAPHONE}"
+                    msg = f"{name} said, **{emoji}   {content}**"
+                    await message.channel.send(msg)
+                    break
