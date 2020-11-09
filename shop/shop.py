2740d80993181220e3f3f6f4a0ee108bcad67ec0
Thalamuszen <thalamuszen@hotmail.com>
2020-11-09 23:06:46 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -961,7 +961,7 @@ class Shop(commands.Cog):
         if item in author_inv:
             pass
         else:
-            return await ctx.send("You don't own this item.")
+            return await self.inventory(ctx)
         info = await self.config.member(ctx.author).inventory.get_raw(item)
         size = info.get("size")	
         giftable = info.get("giftable")
