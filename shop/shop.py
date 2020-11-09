aca0725e45a580a26ecc2dd9d536b2952e219a27
Thalamuszen <thalamuszen@hotmail.com>
2020-11-09 16:58:23 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -1248,8 +1248,8 @@ class Shop(commands.Cog):
         if stuff == []:
             embed.description="Nothing to see here."
         else:
-            headers = ("Item", "Price", "Quantity", "Looks like")
-            output = box(tabulate(stuff, headers=headers, numalign="left"), lang="md")		
+            headers = ("Item", "Price", "Qty", "Looks like")
+            output = box(tabulate(stuff, headers=headers, numalign="right"), lang="md")		
             embed.description=f"`Syntax !buy <quantity> <item_name>`\n\nWhen using `!buy` items are **case sensitive**.\n\n{output}"
         await ctx.send(embed=embed)
 #        await menu(ctx, pages=embed, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=15)
