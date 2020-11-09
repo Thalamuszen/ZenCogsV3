2ade0fed743c2d34d1bf6a9fa39c4ed57526ff1a
Thalamuszen <thalamuszen@hotmail.com>
2020-11-09 21:32:06 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -1280,7 +1280,7 @@ class Shop(commands.Cog):
             quantity = int(xmas.get("quantity"))
             table = [x, priceint, quantity]
             xmas_embed.append(table)
-            sorted_xmas = sorted(xmas_embed, key=itemgetter(1), reverse=true)
+            sorted_xmas = sorted(xmas_embed, key=itemgetter(1), reverse=True)
         if role_embed == []:
             embed_r.description="Nothing to see here."
         else:
