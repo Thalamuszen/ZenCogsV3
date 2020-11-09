11d95218f0ffa989a7dae3da31e9a0a99d61e437
Thalamuszen <thalamuszen@hotmail.com>
2020-11-09 19:18:05 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -1284,28 +1284,28 @@ class Shop(commands.Cog):
         else:
             headers = ("Item", "Price", "Qty", "Looks like")
             output = box(tabulate(role_embed, headers=headers, colalign=("left", "right", "right",)), lang="md")		
-            embed_r.description=f"`Syntax !buy <quantity> <item_name>`\n\nWhen using `!buy` items are **case sensitive**.\n\n{output}"
+            embed_r.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send your friends during the festive period!\nWhen using `!buy` command, keep in mind that items are **case sensitive**.\nAfter purchasing your gift, use the `!gift` command to send them to your friends.\n\n`!buy <quantity> <item_name>`\n{output}"
             embeds.append(embed_r)	
         if item_embed == []:
             embed_i.description="Nothing to see here."
         else:
             headers = ("Item", "Price", "Qty", "Looks like")
             output = box(tabulate(item_embed, headers=headers, colalign=("left", "right", "right",)), lang="md")		
-            embed_i.description=f"`Syntax !buy <quantity> <item_name>`\n\nWhen using `!buy` items are **case sensitive**.\n\n{output}"
+            embed_i.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send your friends during the festive period!\nWhen using `!buy` command, keep in mind that items are **case sensitive**.\nAfter purchasing your gift, use the `!gift` command to send them to your friends.\n\n`!buy <quantity> <item_name>`\n{output}"
             embeds.append(embed_i)
         if game_embed == []:
             embed_g.description="Nothing to see here."
         else:
             headers = ("Item", "Price", "Qty", "Looks like")
             output = box(tabulate(game_embed, headers=headers, colalign=("left", "right", "right",)), lang="md")		
-            embed_g.description=f"`Syntax !buy <quantity> <item_name>`\n\nWhen using `!buy` items are **case sensitive**.\n\n{output}"
+            embed_g.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send your friends during the festive period!\nWhen using `!buy` command, keep in mind that items are **case sensitive**.\nAfter purchasing your gift, use the `!gift` command to send them to your friends.\n\n`!buy <quantity> <item_name>`\n{output}"
             embeds.append(embed_g)
         if xmas_embed == []:
             embed_x.description="Nothing to see here."
         else:
             headers = ("Item", "Price", "Qty", "Looks like")
             output = box(tabulate(xmas_embed, headers=headers, colalign=("left", "right", "right",)), lang="md")		
-            embed_x.description=f"`Syntax !buy <quantity> <item_name>`\n\nWhen using `!buy` items are **case sensitive**.\n\n{output}"	
+            embed_x.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send your friends during the festive period!\nWhen using `!buy` command, keep in mind that items are **case sensitive**.\nAfter purchasing your gift, use the `!gift` command to send them to your friends.\n\n`!buy <quantity> <item_name>`\n{output}"	
             embeds.append(embed_x)
 #        await ctx.send(embed=embed)
         await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=15)
