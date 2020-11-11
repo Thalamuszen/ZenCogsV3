a47090358038483088a611a5a3936ad0b52278ca
Thalamuszen <thalamuszen@hotmail.com>
2020-11-11 13:22:47 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -1321,5 +1321,4 @@ class Shop(commands.Cog):
             output = box(tabulate(sorted_xmas, headers=headers, colalign=("left", "left", "right", "right",)), lang="md")		
             embed_x.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send to your friends for the festive period!\n\nAfter your purchase, use the `!gift` command to gift the item to a friend.\nAfter the **24th of December** the `!open` command will become available. Keep an eye on the Elune Updates channel for more information.\n\n`!buy <quantity> <item_name>` - Item names are case sensitive.\n{output}"	
             embeds.append(embed_x)
-#        await ctx.send(embed=embed)
         await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=120)
