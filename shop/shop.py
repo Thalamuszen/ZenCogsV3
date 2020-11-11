05a96857d5ae8676d2d81c284086aa35130b7094
Thalamuszen <thalamuszen@hotmail.com>
2020-11-11 11:08:18 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -1305,7 +1305,7 @@ class Shop(commands.Cog):
         else:
             headers = ("", "Item", "Price", "Qty")
             output = box(tabulate(sorted_xmas, headers=headers, colalign=("left", "left", "right", "right",)), lang="md")		
-            embed_x.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send your friends for the festive period!\n\nAfter your purchase, use the `!gift` command to gift the item to your friends.\nWhen Christmas is upon us, the `!open` command will be available. Keep an eye on the Elune Updates channel.\n\n`!buy <quantity> <item_name>`\n{output}"	
+            embed_x.description=f"Welcome to Elune's Christmas shop, here you will find gifts to send to your friends for the festive period!\n\nAfter your purchase, use the `!gift` command to gift the item to your friends.\nAfter the **24th of December** the `!open` command will become available. Keep an eye on the Elune Updates channel for more information.\n\n`!buy <quantity> <item_name>`\n{output}"	
             embeds.append(embed_x)
 #        await ctx.send(embed=embed)
         await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=120)
