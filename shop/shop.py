eab0df282d6af5ddbeb34a1c6dcfa2c9f21dedf4
Thalamuszen <thalamuszen@hotmail.com>
2020-11-11 12:34:47 +0000

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -977,7 +977,7 @@ class Shop(commands.Cog):
         if item in author_inv:
             pass
         else:
-            await ctx.send("You don't own this item. Check your inventory below:")
+            await ctx.send(":x: You don't own this item. Items are case sensitive. Check your inventory below:")
             return await self.inventory(ctx)
         info = await self.config.member(ctx.author).inventory.get_raw(item)
         size = info.get("size")	
@@ -1050,7 +1050,7 @@ class Shop(commands.Cog):
         if item in author_inv:
             pass
         else:
-            await ctx.send("You don't own this item. Check your inventory below:")		
+            await ctx.send(":x: You don't own this item. Items are case sensitive. Check your inventory below:")		
             return await self.inventory(ctx)
         info = await self.config.member(ctx.author).inventory.get_raw(item)        
         giftable = info.get("giftable")
