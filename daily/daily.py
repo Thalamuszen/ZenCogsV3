24bf8a4ca76402ad2c41e064f0950b8dfd5bc302
Thalamuszen <thalamuszen@hotmail.com>
2020-12-01 19:15:54 +0000

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -132,7 +132,7 @@ class Daily(commands.Cog):
 
             #now = now.strftime("%Y-%m-%d %H:%M:%S")
             remaining = int((midnight_tomorrow - now).total_seconds())
-            remaining_time = str(timedelta(seconds=remaining))
+            remaining_time = timedelta(seconds=remaining)
             nice_remaining_time = datetime.strftime(remaining_time, "%H:%M:%S")
             embed.description=f"You have already claimed your daily.\nYour next daily will be available in: {nice_remaining_time}."
             await ctx.send(embed=embed)
