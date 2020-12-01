fa58634cf34b519320ee3a3f5ff3210d66db788b
Thalamuszen <thalamuszen@hotmail.com>
2020-12-01 23:34:28 +0000

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -169,9 +169,12 @@ class Daily(commands.Cog):
             remaining_min = remaining_min.lstrip("0")
             if hour == "" and remaining_min == "":
                 minute = ""
-            elif remaining_hour >= "1" and remaining_min == "":
+            elif remaining_hour == "1" and remaining_min == "":
                 hour = "hour"
                 minute = "and"
+            elif remaining_hour > "1" and remaining_min == "":
+                hour = "hours"
+                minute = "and"                
             elif remaining_min == "":
                 minute = "and"      
             elif remaining_min == "1":
